dddcdda236ede8458d4369c035c6a4b7c028b3ef
Jonathan Wade <jnwade@fullsail.edu>
2013-04-28 19:33:06 -0400

    Update

---
+++ js/json.js
@@ -0,0 +1,106 @@
+  
+ 	// ASD 1304 // Mobile Development
+	// Full Sail University
+	// Jonathan Wade
+
+		//127.0.0.1:5984/dutytracker/_all_docs?include_docs=true&start_key="Jon"
+
+{
+		"_id":
+		"techName": [
+			"Tech:",
+			"Jon"
+		],
+		"date": [
+			"Date:", 
+			"12/13/2013"
+		],
+		"inboxCheck1":[
+			"Inbox Check 1:", 
+			"incomplete"
+		],
+		"inboxCheck2":[ 
+			"Inbox Check 2:", 
+			"Complete"
+		],
+		"reportCheck": [
+			"Report Check:", 
+			"Incomplete"
+		],
+		"notes": [
+			"Notes:", 
+			"These are some notes"
+		]
+		 
+	}
+	"log2": {
+		"techName": ["Tech:", "Jon"],
+		"date": ["Date:", "12/13/2013"],
+		"inboxCheck1":["Inbox Check 1:", "The Bee-Gees"],
+		"inboxCheck2":["Inbox Check 2:", "Complete"],
+		"reportCheck": ["Report Check:", "Incomplete"]
+		"notes": ["Notes:", "These are some notes"]
+	},
+	"log3": {
+		"techName": ["Tech:", "Jon"],
+		"date": ["Date:", "12/13/2013"],
+		"inboxCheck1":["Inbox Check 1:", "The Bee-Gees"],
+		"inboxCheck2":["Inbox Check 2:", "Complete"],
+		"reportCheck": ["Report Check:", "Incomplete"]
+		"notes": ["Notes:", "These are some notes"]	
+	},
+	"log4": {
+		"techName": ["Tech:", "Jon"],
+		"date": ["Date:", "12/13/2013"],
+		"inboxCheck1":["Inbox Check 1:", "The Bee-Gees"],
+		"inboxCheck2":["Inbox Check 2:", "Complete"],
+		"reportCheck": ["Report Check:", "Incomplete"]
+		"notes": ["Notes:", "These are some notes"]
+	},
+	"log5": {
+		"techName": ["Tech:", "Jon"],
+		"date": ["Date:", "12/13/2013"],
+		"inboxCheck1":["Inbox Check 1:", "The Bee-Gees"],
+		"inboxCheck2":["Inbox Check 2:", "Complete"],
+		"reportCheck": ["Report Check:", "Incomplete"]
+		"notes": ["Notes:", "These are some notes"]	},
+	"log6": {
+		"techName": ["Tech:", "Jon"],
+		"date": ["Date:", "12/13/2013"],
+		"inboxCheck1":["Inbox Check 1:", "The Bee-Gees"],
+		"inboxCheck2":["Inbox Check 2:", "Complete"],
+		"reportCheck": ["Report Check:", "Incomplete"]
+		"notes": ["Notes:", "These are some notes"]
+	},
+	"techName": ["Tech:", "Jon"],
+		"date": ["Date:", "12/13/2013"],
+		"inboxCheck1":["Inbox Check 1:", "The Bee-Gees"],
+		"inboxCheck2":["Inbox Check 2:", "Complete"],
+		"reportCheck": ["Report Check:", "Incomplete"]
+		"notes": ["Notes:", "These are some notes"]	},
+	"log8": {
+		"techName": ["Tech:", "Jon"],
+		"date": ["Date:", "12/13/2013"],
+		"inboxCheck1":["Inbox Check 1:", "The Bee-Gees"],
+		"inboxCheck2":["Inbox Check 2:", "Complete"],
+		"reportCheck": ["Report Check:", "Incomplete"]
+		"notes": ["Notes:", "These are some notes"]
+	},
+	"log9": {
+		"techName": ["Tech:", "Jon"],
+		"date": ["Date:", "12/13/2013"],
+		"inboxCheck1":["Inbox Check 1:", "The Bee-Gees"],
+		"inboxCheck2":["Inbox Check 2:", "Complete"],
+		"reportCheck": ["Report Check:", "Incomplete"]
+		"notes": ["Notes:", "These are some notes"]
+	},
+	"log10": {
+		"techName": ["Tech:", "Jon"],
+		"date": ["Date:", "12/13/2013"],
+		"inboxCheck1":["Inbox Check 1:", "The Bee-Gees"],
+		"inboxCheck2":["Inbox Check 2:", "Complete"],
+		"reportCheck": ["Report Check:", "Incomplete"]
+		"notes": ["Notes:", "These are some notes"]	
+		};
+	
+
